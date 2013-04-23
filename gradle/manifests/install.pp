8daaae9f197c5fb2ff7c58d58eab8fdabe39e125
Gabriel Monroy <gabriel@opdemand.com>
2013-04-23 11:13:25 -0600

    add support for oracle java with auto-accept of license

--- gradle/manifests/install.pp
+++ gradle/manifests/install.pp
@@ -5,9 +5,25 @@ class gradle::install (
   # webupdat8 PPA to support oracle-* packages (https://launchpad.net/~webupd8team/+archive/java)
   apt::ppa {"ppa:webupd8team/java":}
   
+  # install debconf-utils if not defined already
+  if ! defined(Package["debconf-utils"]) {
+  	package { "debconf-utils":
+  	  ensure => present,
+  	}
+  }
+  
+  # NOTICE: accept oracle licenses automatically
+  exec {"oracle-license-accept":
+    command => "echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections",
+    provider => "shell",
+    unless => "debconf-get-selections | grep shared/accepted-oracle-license-v1-1 | grep true",
+    require => Package["debconf-utils"],
+  }
+  
   # install runtime and tools
   package { $system_packages:
     ensure => present,	 # stability over new hotness
+    require => Exec["oracle-license-accept"],
   }
 
   # create define for upstart template installation
