ff91148b0fa77123ab3e988ec46d04dfef3a3b8f
Gabriel Monroy <gabriel@opdemand.com>
2013-04-22 18:31:40 -0600

    reorder vars by importance

--- opdemand/manifests/app/repository.pp
+++ opdemand/manifests/app/repository.pp
@@ -2,11 +2,10 @@ class opdemand::app::repository (
   
   # inputs default to hiera lookup followed by second arg
   # can override using parameterized class inputs
-  $repository_provider = hiera("APPLICATION_REPOSITORY_PROVIDER", "git"),
-  $repository_key = hiera("APPLICATION_REPOSITORY_KEY", ""),
   $repository_url = hiera("APPLICATION_REPOSITORY_URL"),
   $repository_path = hiera("APPLICATION_REPOSITORY_PATH", "/home/ubuntu/repo"),
   $repository_revision = hiera("APPLICATION_REPOSITORY_REVISION", "master"),
+  $repository_key = hiera("APPLICATION_REPOSITORY_KEY", ""),
   $repository_owner = hiera("APPLICATION_USERNAME", "ubuntu"),
   $repository_group = hiera("APPLICATION_GROUP", "ubuntu")) {
 
