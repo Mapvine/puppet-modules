d206defb0b789b60a4c002cf26fe19db67c4da68
Gabriel Monroy <gabriel@opdemand.com>
2013-04-23 13:27:31 -0600

    remove hardcoded env

--- grails/manifests/grailsw.pp
+++ grails/manifests/grailsw.pp
@@ -12,7 +12,6 @@ class grails::grailsw (
     # log raw output from shell command
     logoutput => true,
     provider => "shell",
-    environment => 'JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")',
     cwd => $repository_path,
     path => ["$repository_path", "$repository_path/bin", "/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
     user => $username,
@@ -28,7 +27,6 @@ class grails::grailsw (
     # log raw output from shell command
     logoutput => true,
     provider => "shell",
-    environment => 'JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")',
     cwd => $repository_path,
     path => ["$repository_path", "$repository_path/bin", "/sbin", "/bin", "/usr/bin", "/usr/local/bin"],
     user => $username,
